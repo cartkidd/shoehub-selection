c686539233404eedae22f6f4a16cd42d530575bc
Cartrell KIDD <cartrellk56@gmail.com>
2022-01-30 13:56:26 -0500

    app.js added

--- server/app.js
+++ server/app.js
@@ -1,34 +1,10 @@
+//Get the express library from node_modules which we have just downloaded.
 const express = require("express");
-const { graphqlHTTP } = require('express-graphql');
-const mongoose = require("mongoose")
 
-const cors = require("cors");
-
-const schema = require("./schema/schema");
-
-
+//Making const of express() into a variable (JS function first class Object).
 const app = express();
-
-app.use(cors());
-
-mongoose.connect(
-    "mongodb://localhost:27017/myapp",
-    { useNewUrlParser: true },
-    () => {
-      console.log("Connect with DB successfully.");
-    }
-  );
-
-app.use(
-    "/graphql",
-    graphqlHTTP({
-      schema: schema,
-      graphiql: true
-    })
-  );
-
 
 //When our application starts, it will listen on port 4000
 app.listen(4000, () => {
-    console.log("Server is listening on port 4000");
-  });
+  console.log("Server is listening on port 4000");
+});
