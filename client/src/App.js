0bc260e1da706f2ee7d42a9f72a38eed9bf0b0da
Cartrell KIDD <cartrellk56@gmail.com>
2022-01-31 16:26:14 -0500

    added ShoeList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ApolloClient from "apollo-boost"; //connect with our server which is running at backend
 import { ApolloProvider } from "react-apollo"; // Connect re
+import CarList from "./components/CarList";
+import ShoeList from "./components/ShoeList";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql"
@@ -11,6 +13,7 @@ function App() {
     <ApolloProvider client={client}>
     <div className="App">
       <h1>List of Shoes</h1>
+      <ShoeList></ShoeList>
     </div>
     </ApolloProvider>
   );
---
+++ client/src/components/ShoeList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ShoeList = props => {
+  return (
+    <>
+      <ul id="shoeList">
+        <li>ShoeName</li>
+      </ul>
+    </>
+  );
+};
+
+export default ShoeList;
